ce1082f2ff0da1f7395784eca5d5ace73b5bb9f2
Ivan Kupryianau <wano9715@gmail.com>
2021-03-29 15:56:53 +0300

    feat: done what-a-seaon task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  throw new CustomError("Not implemented");
-};
+	let newArray = []
+
+	if (Array.isArray(members)) {
+		for (let i = 0; i < members.length; i++) {
+			if (typeof members[i] == 'string') {
+				newArray.push((members[i].trim().charAt(0).toUpperCase()))
+			}
+		}
+		return newArray.sort().join('')
+	} 
+	return false;
+}
+	
+	
+
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+	
+	if (!date) return 'Unable to determine the time of year!';
+
+	if (date instanceof Date) {
+		if (Object.prototype.toString.call(date) !== `[object Date]`) {
+			throw new Error (`Error`)
+		}
+		
+		let newDate = date.getMonth();
+	
+	switch(newDate) {
+		case 2: 
+		case 3: 
+		case 4:
+			return `spring`;
+		case 5: 
+		case 6: 
+		case 7:
+			return `summer`
+		case 8: 
+		case 9: 
+		case 10:
+			return `fall`
+		case 11: 
+		case 0: 
+		case 1:
+			return `winter`
+		
+		}
+	}
+	 throw new Error('Wrong argument');
 };
+
