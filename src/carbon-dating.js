de0b1f9ef2938f5c36fbd1d7e84df06e783d8f00
Ivan Kupryianau <wano9715@gmail.com>
2021-03-29 18:28:20 +0300

    feat: done Carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  throw new CustomError('Not implemented');
-	
+
+	if (typeof sampleActivity !== 'string' || !parseFloat(sampleActivity) || (sampleActivity <= 0 || sampleActivity >= 15) || sampleActivity === Infinity ) return false;
+
+		let numSample = parseFloat(sampleActivity);
+
+	let upper = Math.log(MODERN_ACTIVITY / numSample)
+	let lower = (0.693 / HALF_LIFE_PERIOD);
+	return Math.ceil(result = upper / lower);
 };
